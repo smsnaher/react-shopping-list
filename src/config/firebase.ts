86bdd147503ae19fb3673b4a3af70068821d447b
hsntareq <hsntareq@gmail.com>
2025-08-13 13:49:25 +0600

    Signing in 1

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -26,7 +26,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
