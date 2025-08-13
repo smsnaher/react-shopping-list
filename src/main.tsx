9709b9e581665bb396ee1cd1da0dd2a182211a96
hsntareq <hsntareq@gmail.com>
2025-08-13 12:57:41 +0600

    Fix 404 issue and baseline

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-shopping-list">
       <App />
     </BrowserRouter>
   </StrictMode>,
